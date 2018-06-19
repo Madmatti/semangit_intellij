9a1494fa766b5d74475f029e269ce1f5109942ec
Matthias Böckmann <admin@madmatti.de>
2018-06-19 22:32:06 +0200

    Good file merging

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,31 +1,34 @@
 import com.opencsv.CSVReader;
 
-import org.apache.jena.base.Sys;
 import org.rdfhdt.hdt.enums.RDFNotation;
-import org.rdfhdt.hdt.hdt.HDT;
-import org.rdfhdt.hdt.hdt.HDTManager;
+import org.rdfhdt.hdt.hdt.*;
 import org.rdfhdt.hdt.header.Header;
 import org.rdfhdt.hdt.options.HDTSpecification;
 import org.apache.commons.lang3.StringEscapeUtils;
+import org.rdfhdt.hdt.rdf.RDFParserCallback;
+import org.rdfhdt.hdt.rdf.RDFParserFactory;
+import org.rdfhdt.hdt.tools.RDF2HDT;
 
 
 import java.io.*;
 
 public class MainClass {
 
-    private static final String PREFIX_Semangit = "<http://www.dennis_stinkt_krass.pizza/>";
+    private static final String PREFIX_Semangit = "<http://www.sg.com/ont/>";
     private static final String TAG_Semangit = "semangit:";
     private static final String TAG_Userprefix = "ghuser_";
-    public static void parseUsers(String path)
+    public static void parseUsers(String path, boolean includePrefix)
     {
         try
         {
             CSVReader reader = new CSVReader(new FileReader(path + "users.csv"));
             BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/users.ttl"), 32768);
             String[] nextLine;
-            writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
-            writer.newLine();
-            writer.newLine();
+            if(includePrefix) {
+                writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
+                writer.newLine();
+                writer.newLine();
+            }
             while((nextLine = reader.readNext())!= null)
             {
                 for (int i = 0; i < nextLine.length; i++) {
@@ -104,35 +107,18 @@ public class MainClass {
         }
     }
 
-    public static void rdf2hdtUsers(String path)
-    {
-        String baseURI = "http://example.com/mydataset";
-        String rdfInput = path.concat("rdf/users.ttl");
-        String inputType = "turtle";
 
-        String hdtOutput = path.concat("hdt/users.hdt");
-        try
-        {
-            HDT hdt = HDTManager.generateHDT(rdfInput, baseURI, RDFNotation.parse(inputType), new HDTSpecification(), null);
-            hdt.saveToHDT(hdtOutput, null);
-            hdt.close();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    public static void parseOrganizationMembers(String path)
+    public static void parseOrganizationMembers(String path, boolean includePrefix)
     {
         try {
             CSVReader reader = new CSVReader(new FileReader(path + "organization_members.csv"));
             BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/organization_members.ttl"), 32768);
             String[] nextLine;
-            writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
-            writer.newLine();
-            writer.newLine();
+            if(includePrefix) {
+                writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
+                writer.newLine();
+                writer.newLine();
+            }
             while((nextLine = reader.readNext())!= null) {
                 writer.write("[ a " + TAG_Semangit + "github_organization_join_event;");
                 writer.newLine();
@@ -150,15 +136,48 @@ public class MainClass {
         }
     }
 
-    public static void rdf2hdtOrganizationMembers(String path)
+
+    public static void parseFollowers(String path, boolean includePrefix)
+    {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(path + "followers.csv"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/followers.ttl"), 32768);
+            String[] nextLine;
+            if(includePrefix) {
+                writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
+                writer.newLine();
+                writer.newLine();
+            }
+            while((nextLine = reader.readNext())!= null) {
+                writer.write("[ a " + TAG_Semangit + "github_follow_event;");
+                writer.newLine();
+                writer.write(TAG_Semangit + "github_following_since \"" + nextLine[2] + "\" ;");
+                writer.write(TAG_Semangit + "github_user_or_project 0 ] " + TAG_Semangit + "github_follower " + TAG_Semangit  + TAG_Userprefix + nextLine[1] + ";");
+                writer.newLine();
+                writer.write(TAG_Semangit + "github_follows " + TAG_Semangit  + TAG_Userprefix + nextLine[0] + ".");
+                writer.newLine();
+            }
+            writer.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+    public static void rdf2hdt(String path, String table)
     {
         String baseURI = "http://example.com/mydataset";
-        String rdfInput = path.concat("rdf/organization_members.ttl");
+        String rdfInput = path.concat("rdf/" + table + ".ttl");
         String inputType = "turtle";
 
-        String hdtOutput = path.concat("hdt/organization_members.hdt");
+        String hdtOutput = path.concat("hdt/" + table + ".hdt");
         try
         {
+            //HDTSpecification specification = new HDTSpecification();
+            //specification.set("triples.format", HDTVocabulary.TRIPLES_TYPE_TRIPLESLIST);
             HDT hdt = HDTManager.generateHDT(rdfInput, baseURI, RDFNotation.parse(inputType), new HDTSpecification(), null);
             hdt.saveToHDT(hdtOutput, null);
             hdt.close();
@@ -170,18 +189,94 @@ public class MainClass {
         }
     }
 
+    public static void appendFileToOutput(String directory, String fileName)
+    {
 
-
+        String outPath = directory.concat("combined.ttl");
+        try(BufferedReader br = new BufferedReader(new FileReader(directory.concat(fileName)))) {
+            Writer output = new BufferedWriter(new FileWriter(outPath, true));
+            for(String line; (line = br.readLine()) != null; ) {
+                output.append(line);
+            }
+            output.close();
+            // line is not visible here.
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
     public static void main(String[] args)
     {
         new File(args[0] + "rdf").mkdirs();
         new File(args[0] + "hdt").mkdirs();
-        parseUsers(args[0]);
-        parseOrganizationMembers(args[0]);
 
-        rdf2hdtUsers(args[0]);
-        rdf2hdtOrganizationMembers(args[0]);
+        parseOrganizationMembers(args[0], true);
+        //rdf2hdt(args[0], "organization_members");
+        System.out.println("Organizations parsed.");
+
+        parseFollowers(args[0], false);
+        System.out.println("Followers parsed.");
+
+        parseUsers(args[0], false);
+        System.out.println("Users parsed.");
+
+        try {
+            String correctPath = args[0].concat("rdf/");
+            System.out.println("Appending Org Members");
+            appendFileToOutput(correctPath, "organization_members.ttl");
+
+            System.out.println("Appending followers");
+            appendFileToOutput(correctPath, "followers.ttl");
+
+            System.out.println("Appending users");
+            appendFileToOutput(correctPath, "users.ttl");
+
+            /*
+            System.out.println("Reading organization Members into memory");
+            String file1str = org.apache.commons.io.FileUtils.readFileToString(file1);
+            //String file2str = org.apache.commons.io.FileUtils.readFileToString(file2);
+            file1 = null;
+            System.out.println("Done! Writing to file...");
+            org.apache.commons.io.FileUtils.write(file4, file1str);
+            file1str = "";
+
+            System.out.println("Reading followers into memory");
+            String file2str = org.apache.commons.io.FileUtils.readFileToString(file2);
+            file2 = null;
+            System.out.println("Done! Writing to file...");
+            org.apache.commons.io.FileUtils.write(file4, file2str);
+            file2str = "";
+
+            System.out.println("Reading users into memory");
+            String file3str = org.apache.commons.io.FileUtils.readFileToString(file3);
+            file3 = null;
+            System.out.println("Done! Writing to file...");
+            org.apache.commons.io.FileUtils.write(file4, file3str);
+            file3str = "";*/
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        //rdf2hdt(args[0], "users");
+
+
+        /*String thePath = args[0].concat("hdt/");
+        try {
+            HDT hdt = HDTManager.loadHDT(thePath.concat("organization_members.hdt"));
+            HDTManager.mapHDT(thePath.concat("users.hdt"));
+            RDF2HDT test = new RDF2HDT();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }*/
 
         System.exit(0);
     }
