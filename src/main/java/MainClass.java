5a7e1890311e8dddf11e35b8e85aa7ea9baba317
Matthias Böckmann <admin@madmatti.de>
2018-07-06 11:12:50 +0200

    Removed HDT from project. Added several converters

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,8 @@
 import com.opencsv.CSVReader;
+import org.apache.jena.base.Sys;
 
+
+/*
 import org.rdfhdt.hdt.enums.RDFNotation;
 import org.rdfhdt.hdt.hdt.*;
 import org.rdfhdt.hdt.header.Header;
@@ -8,7 +11,7 @@ import org.apache.commons.lang3.StringEscapeUtils;
 import org.rdfhdt.hdt.rdf.RDFParserCallback;
 import org.rdfhdt.hdt.rdf.RDFParserFactory;
 import org.rdfhdt.hdt.tools.RDF2HDT;
-
+*/
 
 import java.io.*;
 
@@ -17,6 +20,70 @@ public class MainClass {
     private static final String PREFIX_Semangit = "<http://www.sg.com/ont/>";
     private static final String TAG_Semangit = "semangit:";
     private static final String TAG_Userprefix = "ghuser_";
+    private static final String TAG_Repoprefix = "ghrepo_";
+    private static final String TAG_commit = "ghcom_";
+
+
+    public static void parseFollowers(String path, boolean includePrefix)
+    {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(path + "followers.csv"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/followers.ttl"), 32768);
+            String[] nextLine;
+            if(includePrefix) {
+                writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
+                writer.newLine();
+                writer.newLine();
+            }
+            while((nextLine = reader.readNext())!= null) {
+                writer.write("[ a " + TAG_Semangit + "github_follow_event;");
+                writer.newLine();
+                writer.write(TAG_Semangit + "github_following_since \"" + nextLine[2] + "\" ;");
+                writer.write(TAG_Semangit + "github_user_or_project 0 ] " + TAG_Semangit + "github_follower " + TAG_Semangit  + TAG_Userprefix + nextLine[1] + ";");
+                writer.newLine();
+                writer.write(TAG_Semangit + "github_follows " + TAG_Semangit  + TAG_Userprefix + nextLine[0] + ".");
+                writer.newLine();
+            }
+            writer.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+    public static void parseOrganizationMembers(String path, boolean includePrefix)
+    {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(path + "organization_members.csv"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/organization_members.ttl"), 32768);
+            String[] nextLine;
+            if(includePrefix) {
+                writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
+                writer.newLine();
+                writer.newLine();
+            }
+            while((nextLine = reader.readNext())!= null) {
+                writer.write("[ a " + TAG_Semangit + "github_organization_join_event;");
+                writer.newLine();
+                writer.write(TAG_Semangit + "github_organization_joined_at \"" + nextLine[2] + "\" ] " + TAG_Semangit + "github_organization_joined_by " + TAG_Semangit  + TAG_Userprefix + nextLine[1] + ";");
+                writer.newLine();
+                writer.write(TAG_Semangit + "github_organization_is_joined " + TAG_Semangit  + TAG_Userprefix + nextLine[0] + ".");
+                writer.newLine();
+            }
+            writer.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+
     public static void parseUsers(String path, boolean includePrefix)
     {
         try
@@ -108,26 +175,52 @@ public class MainClass {
     }
 
 
-    public static void parseOrganizationMembers(String path, boolean includePrefix)
-    {
+
+    public static void parseCommitParents(String path, boolean includePrefix) {
         try {
-            CSVReader reader = new CSVReader(new FileReader(path + "organization_members.csv"));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/organization_members.ttl"), 32768);
+            CSVReader reader = new CSVReader(new FileReader(path + "commit_parents.csv"));
+            //TODO: Split file and sort?
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/commit_parents.ttl"), 32768);
             String[] nextLine;
-            if(includePrefix) {
+            String[] curLine;
+            if (includePrefix) {
                 writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
                 writer.newLine();
                 writer.newLine();
             }
-            while((nextLine = reader.readNext())!= null) {
-                writer.write("[ a " + TAG_Semangit + "github_organization_join_event;");
+            curLine = reader.readNext();
+            boolean abbreviated = false;
+            while ((nextLine = reader.readNext()) != null) {
+                if(!abbreviated)
+                {
+                    writer.write(TAG_Semangit + TAG_commit + curLine[0] + " " + TAG_Semangit + "commit_has_parent " + TAG_Semangit + TAG_commit + curLine[1]);
+                }
+                else
+                {
+                    writer.write(TAG_Semangit + TAG_commit + curLine[1]); //only specifying next object. subject/predicate are abbreviated
+                }
+                if(curLine[0].equals(nextLine[0]))
+                {
+                    writer.write(","); //abbreviating subject and predicate for next line
+                    abbreviated = true;
+                }
+                else
+                {
+                    writer.write("."); //cannot use turtle abbreviation here
+                    abbreviated = false;
+                }
                 writer.newLine();
-                writer.write(TAG_Semangit + "github_organization_joined_at \"" + nextLine[2] + "\" ] " + TAG_Semangit + "github_organization_joined_by " + TAG_Semangit  + TAG_Userprefix + nextLine[1] + ";");
-                writer.newLine();
-                writer.write(TAG_Semangit + "github_organization_is_joined " + TAG_Semangit  + TAG_Userprefix + nextLine[0] + ".");
-                writer.newLine();
+                curLine = nextLine;
             }
-            writer.close();
+            //handle last line of file
+            if(!abbreviated)
+            {
+                writer.write(TAG_Semangit + TAG_commit + curLine[0] + " " + TAG_Semangit + "commit_has_parent " + TAG_Semangit + TAG_commit + curLine[1]);
+            }
+            else
+            {
+                writer.write(TAG_Semangit + TAG_commit + curLine[1]); //only specifying next object. subject/predicate are abbreviated
+            }
         }
         catch (Exception e)
         {
@@ -136,28 +229,34 @@ public class MainClass {
         }
     }
 
-
-    public static void parseFollowers(String path, boolean includePrefix)
-    {
+    public static void parseCommits(String path, boolean includePrefix) {
         try {
-            CSVReader reader = new CSVReader(new FileReader(path + "followers.csv"));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/followers.ttl"), 32768);
+            CSVReader reader = new CSVReader(new FileReader(path + "commits.csv"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "rdf/commits.ttl"), 32768);
             String[] nextLine;
-            if(includePrefix) {
+            if (includePrefix) {
                 writer.write("@prefix semangit: " + PREFIX_Semangit + " .");
                 writer.newLine();
                 writer.newLine();
             }
-            while((nextLine = reader.readNext())!= null) {
-                writer.write("[ a " + TAG_Semangit + "github_follow_event;");
+            while ((nextLine = reader.readNext()) != null) {
+                /*for (int i = 0; i < nextLine.length; i++) {
+                    nextLine[i] = groovy.json.StringEscapeUtils.escapeJava(nextLine[i]);
+                }*/
+                String commitURI = TAG_Semangit + TAG_commit + nextLine[0];
+                writer.write(  commitURI + " a " + TAG_Semangit + "github_commit;");
                 writer.newLine();
-                writer.write(TAG_Semangit + "github_following_since \"" + nextLine[2] + "\" ;");
-                writer.write(TAG_Semangit + "github_user_or_project 0 ] " + TAG_Semangit + "github_follower " + TAG_Semangit  + TAG_Userprefix + nextLine[1] + ";");
+                writer.write(TAG_Semangit + "commit_sha \"" + nextLine[1] + "\";");
                 writer.newLine();
-                writer.write(TAG_Semangit + "github_follows " + TAG_Semangit  + TAG_Userprefix + nextLine[0] + ".");
+                writer.write(TAG_Semangit + "commit_author " + TAG_Semangit + TAG_Userprefix + nextLine[2] + ";");
+                writer.newLine();
+                writer.write(TAG_Semangit + "commit_committed_by " + TAG_Semangit + TAG_Userprefix + nextLine[3] + ";");
+                writer.newLine();
+                writer.write(TAG_Semangit + "commit_repository " + TAG_Semangit + TAG_Repoprefix + nextLine[4] + ";");
+                writer.newLine();
+                writer.write(TAG_Semangit + "commit_created_at \"" + nextLine[5] + "\".");
                 writer.newLine();
             }
-            writer.close();
         }
         catch (Exception e)
         {
@@ -166,12 +265,39 @@ public class MainClass {
         }
     }
 
+    /**
+     * Files still to be converted:
+     *     commit_comments
+     *     issue_comments
+     *     issue_events
+     *     issue_labels
+     *     issues
+     *     project_commits
+     *     project_members
+     *     projects
+     *     pull_request_comments
+     *     pull_request_commits
+     *     pull_request_history
+     *     pull_requests
+     *     repo_labels
+     *     repo_milestones
+     *     watchers
+     */
 
-    public static void rdf2hdt(String path, String table)
+
+
+
+    //removed! HDT fails to satisfy our needs, as there are too many bugs in the software, making it impossible to merge very large files
+    /*public static void rdf2hdt(String path, String table)
     {
         String baseURI = "http://example.com/mydataset";
         String rdfInput = path.concat("rdf/" + table + ".ttl");
-        String inputType = "turtle";
+        String inputType = "turtle";    commit_comments
+    commit_parents
+    commits
+    issue_comments
+    issue_events
+
 
         String hdtOutput = path.concat("hdt/" + table + ".hdt");
         try
@@ -187,7 +313,7 @@ public class MainClass {
             e.printStackTrace();
             System.exit(1);
         }
-    }
+    }*/
 
     public static void appendFileToOutput(String directory, String fileName)
     {
@@ -197,6 +323,7 @@ public class MainClass {
             Writer output = new BufferedWriter(new FileWriter(outPath, true));
             for(String line; (line = br.readLine()) != null; ) {
                 output.append(line);
+                ((BufferedWriter) output).newLine();
             }
             output.close();
             // line is not visible here.
@@ -223,9 +350,15 @@ public class MainClass {
         parseUsers(args[0], false);
         System.out.println("Users parsed.");
 
+        parseCommits(args[0], false);
+        System.out.println("Committs parsed.");
+
+        parseCommitParents(args[0], false);
+        System.out.println("CommitParents parsed.");
         try {
-            String correctPath = args[0].concat("rdf/");
+            /*String correctPath = args[0].concat("rdf/");
             System.out.println("Appending Org Members");
+            
             appendFileToOutput(correctPath, "organization_members.ttl");
 
             System.out.println("Appending followers");
@@ -233,51 +366,13 @@ public class MainClass {
 
             System.out.println("Appending users");
             appendFileToOutput(correctPath, "users.ttl");
-
-            /*
-            System.out.println("Reading organization Members into memory");
-            String file1str = org.apache.commons.io.FileUtils.readFileToString(file1);
-            //String file2str = org.apache.commons.io.FileUtils.readFileToString(file2);
-            file1 = null;
-            System.out.println("Done! Writing to file...");
-            org.apache.commons.io.FileUtils.write(file4, file1str);
-            file1str = "";
-
-            System.out.println("Reading followers into memory");
-            String file2str = org.apache.commons.io.FileUtils.readFileToString(file2);
-            file2 = null;
-            System.out.println("Done! Writing to file...");
-            org.apache.commons.io.FileUtils.write(file4, file2str);
-            file2str = "";
-
-            System.out.println("Reading users into memory");
-            String file3str = org.apache.commons.io.FileUtils.readFileToString(file3);
-            file3 = null;
-            System.out.println("Done! Writing to file...");
-            org.apache.commons.io.FileUtils.write(file4, file3str);
-            file3str = "";*/
-
+            */
         }
         catch (Exception e)
         {
             e.printStackTrace();
             System.exit(1);
         }
-        //rdf2hdt(args[0], "users");
-
-
-        /*String thePath = args[0].concat("hdt/");
-        try {
-            HDT hdt = HDTManager.loadHDT(thePath.concat("organization_members.hdt"));
-            HDTManager.mapHDT(thePath.concat("users.hdt"));
-            RDF2HDT test = new RDF2HDT();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            System.exit(1);
-        }*/
-
         System.exit(0);
     }
 }
